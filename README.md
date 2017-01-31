215b95e4defe101824cb8cb88da08b77ccb8f510
Taylor Arnett <tarnett1@mac.com>
2017-01-30 23:03:55 -0700

    iteration 1

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+
+The following resources provided information that was used in the assignment
+https://docs.python.org/2/library/urllib.html#urllib.urlencode
+https://docs.python.org/2/library/re.html#search-vs-match
+CMPUT 404 - HTTP Part 1 Lecture Notes by Abram Hindle
+https://snippets.aktagon.com/snippets/72-split-a-url-into-protocol-domain-port-and-uri-using-regular-expressions
+http://regexr.com/ was used to derive the regular expression I use for parsing the url string in this project
--- httpclient.py
+++ httpclient.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
+#
+# Copyright 2017 Taylor Arnett
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,20 +49,22 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # compile the url parsing regular expression once.
+    pattern = re.compile(r"(https?:\/\/)?([^:/]*)(:(\d*))?(.*)?") # inspired by aktagon (Creative Commons Attribution 3.0 License) https://snippets.aktagon.com/snippets/72-split-a-url-into-protocol-domain-port-and-uri-using-regular-expressions
 
     def connect(self, host, port):
-        # use sockets!
-        return None
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+        return client
 
     def get_code(self, data):
-        return None
+        return int(data.split('\r\n\r\n')[0].split('\r\n')[0].split(' ')[1])
 
     def get_headers(self,data):
-        return None
+        return data.split('\r\n\r\n')[0]
 
     def get_body(self, data):
-        return None
+        return data.split('\r\n\r\n')[1]
 
     # read everything from the socket
     def recvall(self, sock):
@@ -61,13 +79,47 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        url_contents = self.parse_url(url)
+        request = 'GET %s HTTP/1.1\r\n' % url_contents.get('path')
+        request += 'Host: %s\r\n' % url_contents.get('host')
+        request += 'Connection: close\r\n'
+        request += 'Accept: %s\r\n\r\n' % '*/*'
+
+        client = self.connect(url_contents.get('host'), int(url_contents.get('port')))
+        client.sendall(request)
+
+        response = self.recvall(client)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        print response
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        content_length = 0
+        payload = ''
+        if args:
+            payload = urllib.urlencode(args)
+            content_length = len(payload)
+
+        url_contents = self.parse_url(url)
+        request = 'POST %s HTTP/1.1\r\n' % url_contents.get('path')
+        request += 'Host: %s\r\n' % url_contents.get('host')
+        request += 'Connection: close\r\n'
+        request += 'Accept: %s\r\n' % '*/*'
+        request += 'Content-Type: application/x-www-form-urlencoded\r\n'
+        request += 'Content-Length: %s\r\n\r\n' % content_length
+        request += payload + '\r\n\r\n'
+
+        client = self.connect(url_contents.get('host'), int(url_contents.get('port')))
+        client.sendall(request)
+
+        response = self.recvall(client)
+
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        print response
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -75,7 +127,28 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
+    def parse_url(self, url):
+        """
+        given a url, this will parse the host, port and path sections using regular expressions
+        :param url: string of the full url to be parsed
+        :return: dict containing the host, port and path
+        """
+        contents = dict(
+            host=None,
+            port=None,
+            path=None,
+        )
+
+        result = self.pattern.match(url)
+
+        contents['host'] = result.group(2)
+        contents['port'] = result.group(4) or 80
+        contents['path'] = result.group(5) or '/'
+
+        return contents
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -85,4 +158,4 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         print client.command( sys.argv[2], sys.argv[1] )
     else:
-        print client.command( sys.argv[1] )   
+        print client.command( sys.argv[1] )
